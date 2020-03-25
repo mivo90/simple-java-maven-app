33343f1b3ee9ac0e8ca6de776f030b8a5fbc4e22
Kwieto_ <guido.thomassen@atos.net>
2020-03-25 11:56:09 +0100

    test

---
+++ src/main/java/com/kwieto/api/controller/Blabla.java
@@ -0,0 +1,12 @@
+package com.kwieto.api.controller;
+
+public class Blabla {
+
+    public boolean isTrue() {
+        return true;
+    }
+
+    public boolean isFalse() {
+        return false;
+    }
+}
