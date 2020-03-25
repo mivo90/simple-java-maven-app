4ebfe334bdda4ff1743fa3e1edcbf00dcd48ae76
Kwieto_ <guido.thomassen@atos.net>
2020-03-25 12:14:25 +0100

    added junit

---
+++ src/test/java/com/kwieto/api/controller/HelloControllerTest.java
@@ -0,0 +1,13 @@
+package com.kwieto.api.controller;
+
+import org.junit.Test;
+
+class HelloControllerTest {
+
+	@Test
+	public void helloTest() {
+		HelloController helloController = new HelloController();
+		helloController.hello();
+	}
+}
+
