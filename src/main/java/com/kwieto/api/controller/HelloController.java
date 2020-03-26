1f5ffd162a46e5adf899a31ce92d2136f46b7351
= <=>
2020-03-26 15:18:18 +0100

    test added text

--- src/main/java/com/kwieto/api/controller/HelloController.java
+++ src/main/java/com/kwieto/api/controller/HelloController.java
@@ -7,7 +7,7 @@ public class HelloController {
 
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public String hello() {
-        return "Ik ben er h-h-helemaal klaar mee ~Guido";
+        return "Ik ben er h-h-helemaal klaar mee ~Guido ";
     }
 
     @RequestMapping(value = "/test", method = RequestMethod.GET)
