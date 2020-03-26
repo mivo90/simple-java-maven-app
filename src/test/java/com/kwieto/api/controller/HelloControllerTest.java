17ce3bcf5a3ec34399d30144d45da9928005d30f
Kwieto_ <guido.thomassen@atos.net>
2020-03-26 11:59:41 +0100

    code coverage added

--- src/test/java/com/kwieto/api/controller/HelloControllerTest.java
+++ src/test/java/com/kwieto/api/controller/HelloControllerTest.java
@@ -1,19 +1,20 @@
 package com.kwieto.api.controller;
 
 import org.junit.Test;
+import org.junit.Assert;
 
 public class HelloControllerTest {
 
 	@Test
 	public void helloTest() {
 		HelloController helloController = new HelloController();
-		helloController.hello();
+		Assert.assertEquals("Hello!", helloController.hello());
 	}
 
 	@Test
 	public void testTest() {
 		HelloController helloController = new HelloController();
-		helloController.test();
+		Assert.assertEquals("Test!", helloController.test());
 	}
 }
 
